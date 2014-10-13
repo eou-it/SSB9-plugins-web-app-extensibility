e238909e216de882d19a0232500fed9f2bb7fe2b
Kevin J. Butler <kevin.butler@ellucian.com>
2014-10-13 10:35:32 -0600

    Merged basic extensibility-remove item for course details

--- web-app/js/extensibility-jquery/xe-jquery.js
+++ web-app/js/extensibility-jquery/xe-jquery.js
@@ -1,5 +1,18 @@
 // jquery/backbone-specific extensibility code
-$(document).ready(function () {
+xe.jq = (function(xe) {
+  var jq = xe.jq || {};
 
-    xe.startup();
-});
+  jq.extendSection = function(sectionElement) {
+        xe.extend(sectionElement, {xeSection:$(sectionElement).data('xe-section') } );
+  };
+
+  jq.extend = function(rootElement) {
+    $(xe.selector('section'), rootElement).each( function(idx, ele) {jq.extendSection(ele);} );
+  }
+
+  $( function() {
+    xe.jq.extend();
+  });
+
+  return jq;
+})(xe||{});
