0f296172bb59e66630aa912ff96a7f8ea31a6e1d
Steve_Lambert <steve.lambert@ellucian.com>
2014-12-23 14:45:08 +0000

    EXTZ-702
    Implemented a rework to allow for section reordering

--- web-app/js/extensibility-jquery/xe-jquery.js
+++ web-app/js/extensibility-jquery/xe-jquery.js
@@ -1,53 +1,49 @@
 // jquery/backbone-specific extensibility code
 xe.jq = (function(xe) {
+
 	var jq = xe.jq || {};
 
-	//Create a dom structure from handlebars templates, do extensions and save the modified templates
-	jq.extendTemplates = function(rootElement) {
-        var templates = $('script[type="text/x-handlebars-template"]',rootElement);
-        templates.each(function (index, template){
-            var rootElement =  $('<div>'+template.text+'</div>');
-            $(rootElement[0].firstElementChild).addClass('xe-extended'); // prevent duplicate application
-            //Group level extensions
-            xe.extendPagePart(rootElement);
-            //Section level extensions
-            $(xe.selector(xe.type.section), rootElement).each(function (index, section) {
-                jq.extendSection(section);
+    /***************************************************************************************************
+    Create a dom structure from handlebars templates, do extensions and save the modified templates
+    ***************************************************************************************************/
+    jq.extendTemplates = function(rootElement) {
+
+        if ( xe.enableExtensions() ) {
+            var templates = $('script[type="text/x-handlebars-template"]',rootElement);
+            templates.each(function (template){
+                var rootElement =  $('<div>'+template.text+'</div>');
+                $(rootElement[0].firstElementChild).addClass('xe-extended'); // prevent duplicate application
+                xe.extend( rootElement );
+                template.text=rootElement[0].innerHTML;
             });
-            template.text=rootElement[0].innerHTML;
-        });
-	};
-
-    jq.extendSection = function(sectionElement) {
-        xe.extend(sectionElement, {xeSection:$(sectionElement).attr(xe.attr.section) } );
-    };
-
-    jq.extend = function(rootElement) {
-	    if (rootElement && $('.xe-extended',rootElement).length) {
-            //the root element has already been extended -- bail out
-            return;
         }
-	    //Group level extensions
-        xe.extendPagePart(rootElement);
-        //Section level extensions
-        var sections = $(xe.selector(xe.type.section), rootElement);
-        //include the top level element if it is a section
-        if ($(rootElement).is(xe.selector(xe.type.section))) {sections = sections.add( rootElement );}
-        sections.each( function(idx, ele) {jq.extendSection(ele);} );
-        //Handlebars template extensions
-        jq.extendTemplates(rootElement);
+    }
+
+
+    /***************************************************************************************************
+    Extensions are specified in the JSON metadata at either the section level or the field level
+    This function processes section level extensions
+    ***************************************************************************************************/
+    jq.extend = function( rootElement ) {
+
+        if ( xe.enableExtensions() ) {
+            xe.extend( rootElement );
+            xe.jq.extendTemplates( rootElement );
+        }
     };
+
 
     $( function() {
-        xe.jq.extend();
+        xe.jq.extend( $('body') );
     });
 
     return jq;
 })(xe||{});
+
 //Reordering of Tabs
 $.fn.tabs = _.wrap($.fn.tabs, function expandTabs(org) {
     var self = this;
-    var sections = xe.extensions.sections;
+    var sections = xe.extensions.orderedSections;
     var list = this.find("ol,ul").eq(0);
     $.each( sections, function(key, section  ) {   //Iterate through array of JSON objects (extensions)
         if(typeof section.nextSibling!=='undefined') {
