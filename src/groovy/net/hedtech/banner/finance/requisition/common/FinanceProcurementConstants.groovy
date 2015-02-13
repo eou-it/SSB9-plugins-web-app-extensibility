d3a34bd471fd8e3af9bdc93f3a3e16c22a71c1b7
stiwari <shiv.tiwari@ellucian.com>
2015-02-13 16:28:05 +0530

    Added field names to constant file for req header

---
+++ src/groovy/net/hedtech/banner/finance/requisition/common/FinanceProcurementConstants.groovy
@@ -0,0 +1,80 @@
+/*******************************************************************************
+ Copyright 2015 Ellucian Company L.P. and its affiliates.
+ *******************************************************************************/
+package net.hedtech.banner.finance.requisition.common
+
+import org.hibernate.annotations.Type
+
+import javax.persistence.Column
+import javax.persistence.Temporal
+import javax.persistence.TemporalType
+import javax.persistence.Version
+
+/**
+ * Util class for Finance Procurement module where in this class will holds all common method and variables will be
+ * used across the banner-finance-procurement plugin.
+ */
+class FinanceProcurementConstants {
+    /* Constant for Requisition Header Domain*/
+
+    public static final def REQUISITION_HEADER_FINDER_BY_REQUEST_CODE = 'RequisitionHeader.fetchByRequestCode'
+    public static final def REQUISITION_HEADER_FINDER_BY_REQUEST_CODE_PARAM_REQUEST_CODE = 'requestCode'
+    public static final def REQUISITION_HEADER_VIEW = 'FPVREQH'
+    public static final def REQUISITION_HEADER_FIELD_FPBREQH_SURROGATE_ID = 'FPBREQH_SURROGATE_ID'
+    public static final def REQUISITION_HEADER_SEQ_FPBREQH_SURROGATE_ID_SEQUENCE = 'FPBREQH_SURROGATE_ID_SEQUENCE'
+    public static final def REQUISITION_HEADER_SEQ_GENERATOR = 'FPBREQH_SEQ_GEN'
+    public static final def REQUISITION_HEADER_FIELD_FPBREQH_CODE = 'FPBREQH_CODE'
+    public static final def REQUISITION_HEADER_FIELD_FPBREQH_ACTIVITY_DATE = 'FPBREQH_ACTIVITY_DATE'
+    public static final def REQUISITION_HEADER_FIELD_FPBREQH_USER_ID = 'FPBREQH_USER_ID'
+    public static final def REQUISITION_HEADER_FIELD_FPBREQH_REQH_DATE = 'FPBREQH_REQH_DATE'
+    public static final def REQUISITION_HEADER_FIELD_FPBREQH_TRANS_DATE = 'FPBREQH_TRANS_DATE'
+    public static final def REQUISITION_HEADER_FIELD_FPBREQH_NAME = 'FPBREQH_NAME'
+    public static final def REQUISITION_HEADER_FIELD_FPBREQH_PHONE_AREA = 'FPBREQH_PHONE_AREA'
+    public static final def REQUISITION_HEADER_FIELD_FPBREQH_PHONE_NUM = 'FPBREQH_PHONE_NUM'
+    public static final def REQUISITION_HEADER_FIELD_FPBREQH_PHONE_EXT = 'FPBREQH_PHONE_EXT'
+    public static final def REQUISITION_HEADER_FIELD_FPBREQH_VEND_PIDM = 'FPBREQH_VEND_PIDM'
+    public static final def REQUISITION_HEADER_FIELD_FPBREQH_ATYP_CODE = 'FPBREQH_ATYP_CODE'
+    public static final def REQUISITION_HEADER_FIELD_FPBREQH_ATYP_SEQ_NUM = 'FPBREQH_ATYP_SEQ_NUM'
+    public static final def REQUISITION_HEADER_FIELD_FPBREQH_COAS_CODE = 'FPBREQH_COAS_CODE'
+    public static final def REQUISITION_HEADER_FIELD_FPBREQH_ORGN_CODE = 'FPBREQH_ORGN_CODE'
+    public static final def REQUISITION_HEADER_FIELD_FPBREQH_REQD_DATE = 'FPBREQH_REQD_DATE'
+    public static final def REQUISITION_HEADER_FIELD_FPBREQH_COMPLETE_IND = 'FPBREQH_COMPLETE_IND'
+    public static final def REQUISITION_HEADER_FIELD_FPBREQH_PRINT_IND = 'FPBREQH_PRINT_IND'
+    public static final def REQUISITION_HEADER_FIELD_FPBREQH_ENCUMB_IND = 'FPBREQH_ENCUMB_IND'
+    public static final def REQUISITION_HEADER_FIELD_FPBREQH_SUSP_IND = 'FPBREQH_SUSP_IND'
+    public static final def REQUISITION_HEADER_FIELD_FPBREQH_CANCEL_IND = 'FPBREQH_CANCEL_IND'
+    public static final def REQUISITION_HEADER_FIELD_FPBREQH_CANCEL_DATE = 'FPBREQH_CANCEL_DATE'
+    public static final def REQUISITION_HEADER_FIELD_FPBREQH_POST_DATE = 'FPBREQH_POST_DATE'
+    public static final def REQUISITION_HEADER_FIELD_FPBREQH_APPR_IND = 'FPBREQH_APPR_IND'
+    public static final def REQUISITION_HEADER_FIELD_FPBREQH_TEXT_IND = 'FPBREQH_TEXT_IND'
+    public static final def REQUISITION_HEADER_FIELD_FPBREQH_EDIT_DEFER_IND = 'FPBREQH_EDIT_DEFER_IND'
+    public static final def REQUISITION_HEADER_FIELD_FPBREQH_RECOMM_VEND_NAME = 'FPBREQH_RECOMM_VEND_NAME'
+    public static final def REQUISITION_HEADER_FIELD_FPBREQH_CURR_CODE = 'FPBREQH_CURR_CODE'
+    public static final def REQUISITION_HEADER_FIELD_FPBREQH_NSF_ON_OFF_IND = 'FPBREQH_NSF_ON_OFF_IND'
+    public static final def REQUISITION_HEADER_FIELD_FPBREQH_SINGLE_ACCTG_IND = 'FPBREQH_SINGLE_ACCTG_IND'
+    public static final def REQUISITION_HEADER_FIELD_FPBREQH_CLOSED_IND = 'FPBREQH_CLOSED_IND'
+    public static final def REQUISITION_HEADER_FIELD_FPBREQH_SHIP_CODE = 'FPBREQH_SHIP_CODE'
+    public static final def REQUISITION_HEADER_FIELD_FPBREQH_RQST_TYPE_IND = 'FPBREQH_RQST_TYPE_IND'
+    public static final def REQUISITION_HEADER_FIELD_FPBREQH_INVENTORY_REQ_IND = 'FPBREQH_INVENTORY_REQ_IND'
+    public static final def REQUISITION_HEADER_FIELD_FPBREQH_CRSN_CODE = 'FPBREQH_CRSN_CODE'
+    public static final def REQUISITION_HEADER_FIELD_FPBREQH_DELIVERY_COMMENT = 'FPBREQH_DELIVERY_COMMENT'
+    public static final def REQUISITION_HEADER_FIELD_FPBREQH_EMAIL_ADDR = 'FPBREQH_EMAIL_ADDR'
+    public static final def REQUISITION_HEADER_FIELD_FPBREQH_FAX_AREA = 'FPBREQH_FAX_AREA'
+    public static final def REQUISITION_HEADER_FIELD_FPBREQH_FAX_NUMBER = 'FPBREQH_FAX_NUMBER'
+    public static final def REQUISITION_HEADER_FIELD_FPBREQH_FAX_EXT = 'FPBREQH_FAX_EXT'
+    public static final def REQUISITION_HEADER_FIELD_FPBREQH_ATTENTION_TO = 'FPBREQH_ATTENTION_TO'
+    public static final def REQUISITION_HEADER_FIELD_FPBREQH_VENDOR_CONTACT = 'FPBREQH_VENDOR_CONTACT'
+    public static final def REQUISITION_HEADER_FIELD_FPBREQH_DISC_CODE = 'FPBREQH_DISC_CODE'
+    public static final def REQUISITION_HEADER_FIELD_FPBREQH_VEND_EMAIL_ADDR = 'FPBREQH_VEND_EMAIL_ADDR'
+    public static final def REQUISITION_HEADER_FIELD_FPBREQH_COPIED_FROM = 'FPBREQH_COPIED_FROM'
+    public static final def REQUISITION_HEADER_FIELD_FPBREQH_TGRP_CODE = 'FPBREQH_TGRP_CODE'
+    public static final def REQUISITION_HEADER_FIELD_FPBREQH_REQ_PRINT_DATE = 'FPBREQH_REQ_PRINT_DATE'
+    public static final def REQUISITION_HEADER_FIELD_FPBREQH_CLOSED_DATE = 'FPBREQH_CLOSED_DATE'
+    public static final def REQUISITION_HEADER_FIELD_FPBREQH_MATCH_REQUIRED = 'FPBREQH_MATCH_REQUIRED'
+    public static final def REQUISITION_HEADER_FIELD_FPBREQH_ORIGIN_CODE = 'FPBREQH_ORIGIN_CODE'
+    public static final def REQUISITION_HEADER_FIELD_FPBREQH_DOC_REF_CODE = 'FPBREQH_DOC_REF_CODE'
+    public static final def REQUISITION_HEADER_FIELD_FPBREQH_CTRY_CODE_PHONE = 'FPBREQH_CTRY_CODE_PHONE'
+    public static final def REQUISITION_HEADER_FIELD_FPBREQH_CTRY_CODE_FAX = 'FPBREQH_CTRY_CODE_FAX'
+    public static final def REQUISITION_HEADER_FIELD_FPBREQH_VERSION = 'FPBREQH_VERSION'
+    public static final def REQUISITION_HEADER_FIELD_FPBREQH_DATA_ORIGIN = 'FPBREQH_DATA_ORIGIN'
+}
--- src/groovy/net/hedtech/banner/finance/requisition/system/RequisitionHeader.groovy
+++ src/groovy/net/hedtech/banner/finance/requisition/system/RequisitionHeader.groovy
@@ -6,13 +6,14 @@ package net.hedtech.banner.finance.requisition.system
 
 import groovy.transform.EqualsAndHashCode
 import groovy.transform.ToString
+import net.hedtech.banner.finance.requisition.common.FinanceProcurementConstants
 import net.hedtech.banner.service.DatabaseModifiesState
 import org.hibernate.annotations.Type
 
 import javax.persistence.*
 
 @NamedQueries(value = [
-        @NamedQuery(name = "RequisitionHeader.fetchByRequestCode",
+        @NamedQuery(name = FinanceProcurementConstants.REQUISITION_HEADER_FINDER_BY_REQUEST_CODE,
                 query = """FROM RequisitionHeader a
              WHERE a.requestCode = :requestCode
    """)
@@ -22,7 +23,7 @@ import javax.persistence.*
  *  Requisition Header Domain object to store the details of requisition header information
  */
 @Entity
-@Table(name = "FPVREQH")
+@Table(name = FinanceProcurementConstants.REQUISITION_HEADER_VIEW)
 @EqualsAndHashCode(includeFields = true)
 @ToString(includeNames = true, ignoreNulls = true)
 @DatabaseModifiesState
@@ -33,99 +34,100 @@ class RequisitionHeader implements Serializable {
      * Surrogate ID for FPBREQH
      */
     @Id
-    @Column(name = "FPBREQH_SURROGATE_ID")
-    @SequenceGenerator(name = "FPBREQH_SEQ_GEN", allocationSize = 1, sequenceName = "FPBREQH_SURROGATE_ID_SEQUENCE")
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "FPBREQH_SEQ_GEN")
+    @Column(name = FinanceProcurementConstants.REQUISITION_HEADER_FIELD_FPBREQH_SURROGATE_ID)
+    @SequenceGenerator(name = FinanceProcurementConstants.REQUISITION_HEADER_SEQ_GENERATOR, allocationSize = 1, sequenceName =
+            FinanceProcurementConstants.REQUISITION_HEADER_SEQ_FPBREQH_SURROGATE_ID_SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = FinanceProcurementConstants.REQUISITION_HEADER_SEQ_GENERATOR)
     Long id
 
     /**
      * CODE
      */
-    @Column(name = "FPBREQH_CODE")
+    @Column(name = FinanceProcurementConstants.REQUISITION_HEADER_FIELD_FPBREQH_CODE)
     String requestCode
 
     /**
      * Activity Date
      */
-    @Column(name = "FPBREQH_ACTIVITY_DATE")
+    @Column(name = FinanceProcurementConstants.REQUISITION_HEADER_FIELD_FPBREQH_ACTIVITY_DATE)
     @Temporal(TemporalType.TIMESTAMP)
     Date lastModified
 
     /**
      * Last Modified By column for FPBREQH
      */
-    @Column(name = "FPBREQH_USER_ID")
+    @Column(name = FinanceProcurementConstants.REQUISITION_HEADER_FIELD_FPBREQH_USER_ID)
     String userId
 
     /**
      * REQUISITION DATE:
      */
-    @Column(name = "FPBREQH_REQH_DATE")
+    @Column(name = FinanceProcurementConstants.REQUISITION_HEADER_FIELD_FPBREQH_REQH_DATE)
     @Temporal(TemporalType.DATE)
     Date requestDate
 
     /**
      * TRANSACTION DATE:  The date the transaction was processed.
      */
-    @Column(name = "FPBREQH_TRANS_DATE")
+    @Column(name = FinanceProcurementConstants.REQUISITION_HEADER_FIELD_FPBREQH_TRANS_DATE)
     @Temporal(TemporalType.DATE)
     Date transactionDate
 
     /**
      * INSTITUTION NAME:  The name of the separate accountable institution.
      */
-    @Column(name = "FPBREQH_NAME")
+    @Column(name = FinanceProcurementConstants.REQUISITION_HEADER_FIELD_FPBREQH_NAME)
     String institutionName
 
     /**
      * PHONE AREA:  The telephone area code for this particular record.
      */
-    @Column(name = "FPBREQH_PHONE_AREA")
+    @Column(name = FinanceProcurementConstants.REQUISITION_HEADER_FIELD_FPBREQH_PHONE_AREA)
     String phoneArea
 
     /**
      * SHIP TO PHONE NUMBER:  The ship to phone number.
      */
-    @Column(name = "FPBREQH_PHONE_NUM")
+    @Column(name = FinanceProcurementConstants.REQUISITION_HEADER_FIELD_FPBREQH_PHONE_NUM)
     String shipToPhoneNumber
 
     /**
      * PHONE EXTENSION:  The telephone extension number for this particular record.
      */
-    @Column(name = "FPBREQH_PHONE_EXT")
+    @Column(name = FinanceProcurementConstants.REQUISITION_HEADER_FIELD_FPBREQH_PHONE_EXT)
     String phoneExtension
 
     /**
      * VENDOR PERSONAL IDENTIFICATION MASTER:  The unique personal identification master of a vendor.
      */
-    @Column(name = "FPBREQH_VEND_PIDM")
+    @Column(name = FinanceProcurementConstants.REQUISITION_HEADER_FIELD_FPBREQH_VEND_PIDM)
     Integer vendorPidm
 
     /**
      * ACCOUNT TYPE CODE:  Classifies an account type i.e., asset, liabilities, control, fund balance, revenue, and labor expenses are used for reporting       purposes.
      */
-    @Column(name = "FPBREQH_ATYP_CODE")
+    @Column(name = FinanceProcurementConstants.REQUISITION_HEADER_FIELD_FPBREQH_ATYP_CODE)
     String accountType
 
     /**
      * ACCOUNT TYPE SEQUENCE NUMBER:  The internal account type that is predefined on the FTVSDAT table.  This number is used for collecting data on financial     reports.
      */
-    @Column(name = "FPBREQH_ATYP_SEQ_NUM")
+    @Column(name = FinanceProcurementConstants.REQUISITION_HEADER_FIELD_FPBREQH_ATYP_SEQ_NUM)
     Integer accountTypeSequenceNumber
 
     /**
      * CHART OF ACCOUNTS CODE:  The primary identification code for any chart of accounts that uniquely identifies that chart from any other in a multi-chart    environment.
      */
-    @Column(name = "FPBREQH_COAS_CODE")
+    @Column(name = FinanceProcurementConstants.REQUISITION_HEADER_FIELD_FPBREQH_COAS_CODE)
     String chartOfAccount
 
     /**
      * ORGANIZATION CODE:  Identifies the organization code that appears on a transaction.
      */
-    @Column(name = "FPBREQH_ORGN_CODE")
+    @Column(name = FinanceProcurementConstants.REQUISITION_HEADER_FIELD_FPBREQH_ORGN_CODE)
     String organization
 
-    @Column(name = "FPBREQH_REQD_DATE")
+    @Column(name = FinanceProcurementConstants.REQUISITION_HEADER_FIELD_FPBREQH_REQD_DATE)
     @Temporal(TemporalType.DATE)
     Date deliveryDate
 
@@ -133,45 +135,45 @@ class RequisitionHeader implements Serializable {
      * COMPLETE INDICATOR:  Indicates if the document has been completed.
      */
     @Type(type = "yes_no")
-    @Column(name = "FPBREQH_COMPLETE_IND")
+    @Column(name = FinanceProcurementConstants.REQUISITION_HEADER_FIELD_FPBREQH_COMPLETE_IND)
     Boolean completeIndicator
 
     /**
      * PRINT INDICATOR
      */
-    @Column(name = "FPBREQH_PRINT_IND")
+    @Column(name = FinanceProcurementConstants.REQUISITION_HEADER_FIELD_FPBREQH_PRINT_IND)
     String printIndicator
 
     /**
      * ENCUMBRANCE INDICATOR:
      */
-    @Column(name = "FPBREQH_ENCUMB_IND")
+    @Column(name = FinanceProcurementConstants.REQUISITION_HEADER_FIELD_FPBREQH_ENCUMB_IND)
     String encumbranceIndicator
 
     /**
      * SUSPENSE INDICATOR:  An indicator used to inform the user that a document is being held in suspense due to missing or erroneous data content.
      */
     @Type(type = "yes_no")
-    @Column(name = "FPBREQH_SUSP_IND")
+    @Column(name = FinanceProcurementConstants.REQUISITION_HEADER_FIELD_FPBREQH_SUSP_IND)
     Boolean suspenseIndicator
 
     /**
      * CANCEL INDICATOR:  The indicator associated with the check cancellation processing.
      */
-    @Column(name = "FPBREQH_CANCEL_IND")
+    @Column(name = FinanceProcurementConstants.REQUISITION_HEADER_FIELD_FPBREQH_CANCEL_IND)
     String cancelIndicator
 
     /**
      * CANCELLATION DATE:  The date that a check was cancelled.
      */
-    @Column(name = "FPBREQH_CANCEL_DATE")
+    @Column(name = FinanceProcurementConstants.REQUISITION_HEADER_FIELD_FPBREQH_CANCEL_DATE)
     @Temporal(TemporalType.DATE)
     Date cancellationDate
 
     /**
      * POSTING DATE:
      */
-    @Column(name = "FPBREQH_POST_DATE")
+    @Column(name = FinanceProcurementConstants.REQUISITION_HEADER_FIELD_FPBREQH_POST_DATE)
     @Temporal(TemporalType.DATE)
     Date postingDate
 
@@ -179,202 +181,202 @@ class RequisitionHeader implements Serializable {
      * APPROVAL INDICATOR:  An indicator that tells whether the document has been approved. This value is set to Y on final approval.
      */
     @Type(type = "yes_no")
-    @Column(name = "FPBREQH_APPR_IND")
+    @Column(name = FinanceProcurementConstants.REQUISITION_HEADER_FIELD_FPBREQH_APPR_IND)
     Boolean approvalIndicator
 
     /**
      * TEXT INDICATOR:  The view text or enter text indicator.
      */
     @Type(type = "yes_no")
-    @Column(name = "FPBREQH_TEXT_IND")
+    @Column(name = FinanceProcurementConstants.REQUISITION_HEADER_FIELD_FPBREQH_TEXT_IND)
     Boolean textIndicator
 
     /**
      * DEFER EDIT INDICATOR:  This indicator is used to defer editing of the transaction until posting.
      */
     @Type(type = "yes_no")
-    @Column(name = "FPBREQH_EDIT_DEFER_IND")
+    @Column(name = FinanceProcurementConstants.REQUISITION_HEADER_FIELD_FPBREQH_EDIT_DEFER_IND)
     Boolean deferEditIndicator
 
     /**
      * Header recommended vendor name
      */
-    @Column(name = "FPBREQH_RECOMM_VEND_NAME")
+    @Column(name = FinanceProcurementConstants.REQUISITION_HEADER_FIELD_FPBREQH_RECOMM_VEND_NAME)
     String recommVendName
 
     /**
      * CURRENCY CODE: The currency code associated with this requisition.
      */
-    @Column(name = "FPBREQH_CURR_CODE")
+    @Column(name = FinanceProcurementConstants.REQUISITION_HEADER_FIELD_FPBREQH_CURR_CODE)
     String currency
 
     /**
      * NSF ON OFF INDICATOR: If Y, available balance checking will be done at entry time for this particular requisition.
      */
     @Type(type = "yes_no")
-    @Column(name = "FPBREQH_NSF_ON_OFF_IND")
+    @Column(name = FinanceProcurementConstants.REQUISITION_HEADER_FIELD_FPBREQH_NSF_ON_OFF_IND)
     Boolean nsfOnOffIndicator
 
     /**
      * SINGLE ACCTG DISTR INDICATOR: If Y, the document will have an accounting or accounting distributions for the document not for specific commodities
      */
     @Type(type = "yes_no")
-    @Column(name = "FPBREQH_SINGLE_ACCTG_IND")
+    @Column(name = FinanceProcurementConstants.REQUISITION_HEADER_FIELD_FPBREQH_SINGLE_ACCTG_IND)
     Boolean singleAcctgDistrIndicator
 
     /**
      * CLOSED INDICATOR: If Y, all items for this requisition are closed, and no more activity will be done against this requisition.
      */
     @Type(type = "yes_no")
-    @Column(name = "FPBREQH_CLOSED_IND")
+    @Column(name = FinanceProcurementConstants.REQUISITION_HEADER_FIELD_FPBREQH_CLOSED_IND)
     Boolean closedIndicator
 
     /**
      * SHIP CODE: The ship_to code of the desired delivery location. Validated against the Ship To Validation table (FTVSHIP).
      */
-    @Column(name = "FPBREQH_SHIP_CODE")
+    @Column(name = FinanceProcurementConstants.REQUISITION_HEADER_FIELD_FPBREQH_SHIP_CODE)
     String ship
 
     /**
      * REQUEST TYPE INDICATOR: Type which defines the kind of requisition this document is.  Valid types are (P)rocurement and (S)tores requisition.
      */
-    @Column(name = "FPBREQH_RQST_TYPE_IND")
+    @Column(name = FinanceProcurementConstants.REQUISITION_HEADER_FIELD_FPBREQH_RQST_TYPE_IND)
     String requestTypeIndicator
 
     /**
      * INVENTORY REQUISITION INDICATOR: Indicator set by the com for an inventory requisition.  Valid values are (Y) Inventory Requisition and (NULL) otherwise. The value is determined by the use of General Ledger Accounts on the requisition.
      */
     @Type(type = "yes_no")
-    @Column(name = "FPBREQH_INVENTORY_REQ_IND")
+    @Column(name = FinanceProcurementConstants.REQUISITION_HEADER_FIELD_FPBREQH_INVENTORY_REQ_IND)
     Boolean inventoryRequisitionIndicator
 
     /**
      * REASON CODE: Cancel reason code.
      */
-    @Column(name = "FPBREQH_CRSN_CODE")
+    @Column(name = FinanceProcurementConstants.REQUISITION_HEADER_FIELD_FPBREQH_CRSN_CODE)
     String reason
 
     /**
      * DELIVERY COMMENT: Comments for delivery date.
      */
-    @Column(name = "FPBREQH_DELIVERY_COMMENT")
+    @Column(name = FinanceProcurementConstants.REQUISITION_HEADER_FIELD_FPBREQH_DELIVERY_COMMENT)
     String deliveryComment
 
     /**
      * EMAIL ADDRESS: Requestor email address.
      */
-    @Column(name = "FPBREQH_EMAIL_ADDR")
+    @Column(name = FinanceProcurementConstants.REQUISITION_HEADER_FIELD_FPBREQH_EMAIL_ADDR)
     String emailAddress
 
     /**
      * FAX AREA CODE: Fax number area code.
      */
-    @Column(name = "FPBREQH_FAX_AREA")
+    @Column(name = FinanceProcurementConstants.REQUISITION_HEADER_FIELD_FPBREQH_FAX_AREA)
     String faxArea
 
     /**
      * FAX NUMBER: Ship To fax telephone number.
      */
-    @Column(name = "FPBREQH_FAX_NUMBER")
+    @Column(name = FinanceProcurementConstants.REQUISITION_HEADER_FIELD_FPBREQH_FAX_NUMBER)
     String faxNumber
 
     /**
      * FAX EXTENTION: Fax number extension.
      */
-    @Column(name = "FPBREQH_FAX_EXT")
+    @Column(name = FinanceProcurementConstants.REQUISITION_HEADER_FIELD_FPBREQH_FAX_EXT)
     String faxExtension
 
     /**
      * ATTENTION TO: Attention To line for ship to address.
      */
-    @Column(name = "FPBREQH_ATTENTION_TO")
+    @Column(name = FinanceProcurementConstants.REQUISITION_HEADER_FIELD_FPBREQH_ATTENTION_TO)
     String attentionTo
 
     /**
      * VENDOR CONTACT: The vendor contact.
      */
-    @Column(name = "FPBREQH_VENDOR_CONTACT")
+    @Column(name = FinanceProcurementConstants.REQUISITION_HEADER_FIELD_FPBREQH_VENDOR_CONTACT)
     String vendorContact
 
     /**
      * DISCOUNT CODE: Discount code assigned to vendor.
      */
-    @Column(name = "FPBREQH_DISC_CODE")
+    @Column(name = FinanceProcurementConstants.REQUISITION_HEADER_FIELD_FPBREQH_DISC_CODE)
     String discount
 
     /**
      * EMAIL ADDRESS: Requestor email address.
      */
-    @Column(name = "FPBREQH_VEND_EMAIL_ADDR")
+    @Column(name = FinanceProcurementConstants.REQUISITION_HEADER_FIELD_FPBREQH_VEND_EMAIL_ADDR)
     String vendorEmailAddress
 
     /**
      * DOCUMENT COPIED FROM: Number of requisition document copied.
      */
-    @Column(name = "FPBREQH_COPIED_FROM")
+    @Column(name = FinanceProcurementConstants.REQUISITION_HEADER_FIELD_FPBREQH_COPIED_FROM)
     String documentCopiedFrom
 
     /**
      * TAX GROUP CODE: The tax group code used with a ship-to-address to determine the taxes to be computed.
      */
-    @Column(name = "FPBREQH_TGRP_CODE")
+    @Column(name = FinanceProcurementConstants.REQUISITION_HEADER_FIELD_FPBREQH_TGRP_CODE)
     String taxGroup
 
     /**
      * REQUISITION PRINT DATE: The date on which the requisition was printed.
      */
-    @Column(name = "FPBREQH_REQ_PRINT_DATE")
+    @Column(name = FinanceProcurementConstants.REQUISITION_HEADER_FIELD_FPBREQH_REQ_PRINT_DATE)
     @Temporal(TemporalType.DATE)
     Date requisitionPrintDate
 
     /**
      * CLOSED DATE: The date that the requisition is closed.
      */
-    @Column(name = "FPBREQH_CLOSED_DATE")
+    @Column(name = FinanceProcurementConstants.REQUISITION_HEADER_FIELD_FPBREQH_CLOSED_DATE)
     @Temporal(TemporalType.DATE)
     Date closedDate
 
     /**
      * Match Required: Used to indicate whether Invoices associated with this Requisition (through Purchase Orders) will require Matching..  Valid values are [N]o matching, [Y]es matching, [U] for no matching at all set on FOBSYSC.
      */
-    @Column(name = "FPBREQH_MATCH_REQUIRED")
+    @Column(name = FinanceProcurementConstants.REQUISITION_HEADER_FIELD_FPBREQH_MATCH_REQUIRED)
     String matchRequired
 
     /**
      * REQUISITION ORIGINATION CODE: This will be set to EPROC-SYSTEM CODE if the eRequisition has been created via an eprocurement com.  The default is BANNER or SELF_SERVICE.
      */
-    @Column(name = "FPBREQH_ORIGIN_CODE")
+    @Column(name = FinanceProcurementConstants.REQUISITION_HEADER_FIELD_FPBREQH_ORIGIN_CODE)
     String requisitionOrigination
 
     /**
      * REQUISITION REFERENCE CODE: This code is the third party eproc com requisition number.  Banner will always create a new requisition number and store this reference.
      */
-    @Column(name = "FPBREQH_DOC_REF_CODE")
+    @Column(name = FinanceProcurementConstants.REQUISITION_HEADER_FIELD_FPBREQH_DOC_REF_CODE)
     String requisitionReference
 
     /**
      * TELEPHONE COUNTRY CODE: Code designating the region or country.
      */
-    @Column(name = "FPBREQH_CTRY_CODE_PHONE")
+    @Column(name = FinanceProcurementConstants.REQUISITION_HEADER_FIELD_FPBREQH_CTRY_CODE_PHONE)
     String telephoneCountry
 
     /**
      * FAX COUNTRY CODE: Code designating the region or country.
      */
-    @Column(name = "FPBREQH_CTRY_CODE_FAX")
+    @Column(name = FinanceProcurementConstants.REQUISITION_HEADER_FIELD_FPBREQH_CTRY_CODE_FAX)
     String faxCountry
 
     /**
      * Version column which is used as an optimistic lock token for FPBREQH
      */
     @Version
-    @Column(name = "FPBREQH_VERSION")
+    @Column(name = FinanceProcurementConstants.REQUISITION_HEADER_FIELD_FPBREQH_VERSION)
     Long version
 
     /**
      * Data Origin column for FPBREQH
      */
-    @Column(name = "FPBREQH_DATA_ORIGIN")
+    @Column(name = FinanceProcurementConstants.REQUISITION_HEADER_FIELD_FPBREQH_DATA_ORIGIN)
     String dataOrigin
 
     static constraints = {
@@ -439,7 +441,8 @@ class RequisitionHeader implements Serializable {
 
     public static RequisitionHeader fetchByRequestCode( String requestCode ) {
         RequisitionHeader requestHeader = RequisitionHeader.withSession {session ->
-            session.getNamedQuery( "RequisitionHeader.fetchByRequestCode" ).setString( 'requestCode', requestCode ).list()[0]
+            session.getNamedQuery( FinanceProcurementConstants.REQUISITION_HEADER_FINDER_BY_REQUEST_CODE ).setString(
+                    FinanceProcurementConstants.REQUISITION_HEADER_FINDER_BY_REQUEST_CODE_PARAM_REQUEST_CODE, requestCode ).list()[0]
         }
         return requestHeader
     }
