45f555bdfee7d8227b4aa554e768c4d12e37b506
Ron Boukens <Ron.Boukens@ellucian.com>
2015-03-10 14:31:27 +0100

    EXTZ-769: RTL entries for ExtensibilityResources.groovy

--- grails-app/conf/ExtensibilityResources.groovy
+++ grails-app/conf/ExtensibilityResources.groovy
@@ -15,8 +15,13 @@ modules = {
         dependsOn "extensibilityCommon"
         resource url: [plugin: 'web-app-extensibility', file: 'js/extensibility-jquery/xe-jquery.js']
     }
+    'extensibilityAngularRTL' {
+        dependsOn "extensibilityAngular, extensibilityCommonRTL"
+    }
     'extensibilityJQueryRTL' {
-        dependsOn "extensibilityJQuery"
+        dependsOn "extensibilityJQuery, extensibilityCommonRTL"
+    }
+    'extensibilityCommonRTL' {
         resource url: [plugin: 'web-app-extensibility', file: 'css/extensibility-ss-rtl.css'], attrs: [media: 'screen, projection']
         resource url: [plugin: 'web-app-extensibility', file: 'css/extensibility-ss-rtl-patch.css'], attrs: [media: 'screen, projection']
     }
