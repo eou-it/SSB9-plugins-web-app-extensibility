62052db6f1adae5fe233b9bb137d64566f06bc66
Arun Upadhya <Arun.Upadhya@ellucian.com>
2015-05-26 18:11:04 +0530

    modified for fixing bug for FOAPAL defaulting

--- grails-app/services/net/hedtech/banner/finance/requisition/system/RequisitionAccountingCompositeService.groovy
+++ grails-app/services/net/hedtech/banner/finance/requisition/system/RequisitionAccountingCompositeService.groovy
@@ -14,15 +14,15 @@ import org.apache.log4j.Logger
  * Class for Purchase Requisition Accounting Composite
  */
 class RequisitionAccountingCompositeService {
-    private static final Logger LOGGER = Logger.getLogger( this.class )
+    private static final Logger LOGGER = Logger.getLogger(this.class)
     boolean transactional = true
 
     def requisitionHeaderService
     def springSecurityService
     def requisitionAccountingService
     def chartOfAccountsService
-    def accountIndexService
-    def financeFundService
+    def financeAccountIndexService
+    def financeFundCompositeService
     def financeOrganizationCompositeService
     def financeAccountCompositeService
     def programService
@@ -36,13 +36,13 @@ class RequisitionAccountingCompositeService {
      * @param map Map which have all the required date to create requisition accounting.
      * @return map Map having requestCode, item and sequenceNumber of created requisition accounting.
      */
-    def createPurchaseRequisitionAccounting( map ) {
+    def createPurchaseRequisitionAccounting(map) {
         RequisitionAccounting requisitionAccountingRequest = map.requisitionAccounting
         def user = springSecurityService.getAuthentication()?.user
         if (user?.oracleUserName) {
             requisitionAccountingRequest.userId = user.oracleUserName
-            def header = requisitionHeaderService.findRequisitionHeaderByRequestCode( requisitionAccountingRequest.requestCode )
-            FinanceProcurementHelper.checkCompleteRequisition( header )
+            def header = requisitionHeaderService.findRequisitionHeaderByRequestCode(requisitionAccountingRequest.requestCode)
+            FinanceProcurementHelper.checkCompleteRequisition(header)
             if (header?.isDocumentLevelAccounting) {
                 requisitionAccountingRequest.item = 0
             }
@@ -52,8 +52,8 @@ class RequisitionAccountingCompositeService {
                         RequisitionAccountingCompositeService,
                         new BusinessLogicValidationException( FinanceProcurementConstants.ERROR_MESSAGE_NO_MORE_ACCOUNTING, [] ) )
             }*/
-            requisitionAccountingRequest.sequenceNumber = requisitionAccountingService.getLastSequenceNumberByRequestCode( requisitionAccountingRequest.requestCode, requisitionAccountingRequest.item ).next()
-            RequisitionAccounting requisitionAccounting = requisitionAccountingService.create( [domainModel: requisitionAccountingRequest] )
+            requisitionAccountingRequest.sequenceNumber = requisitionAccountingService.getLastSequenceNumberByRequestCode(requisitionAccountingRequest.requestCode, requisitionAccountingRequest.item).next()
+            RequisitionAccounting requisitionAccounting = requisitionAccountingService.create([domainModel: requisitionAccountingRequest])
             LoggerUtility.debug LOGGER, 'Requisition Accounting created ' + requisitionAccounting
             return [requestCode: requisitionAccounting.requestCode,
                     item       : requisitionAccounting.item, sequenceNumber: requisitionAccounting.sequenceNumber]
@@ -61,7 +61,7 @@ class RequisitionAccountingCompositeService {
             LoggerUtility.error LOGGER, 'User' + user + ' is not valid'
             throw new ApplicationException(
                     RequisitionAccountingCompositeService,
-                    new BusinessLogicValidationException( FinanceProcurementConstants.ERROR_MESSAGE_USER_NOT_VALID, [] ) )
+                    new BusinessLogicValidationException(FinanceProcurementConstants.ERROR_MESSAGE_USER_NOT_VALID, []))
         }
     }
 
@@ -71,10 +71,10 @@ class RequisitionAccountingCompositeService {
      * @param item Item.
      * @param sequenceNumber Sequence number.
      */
-    def deletePurchaseRequisitionAccountingInformation( requestCode, Integer item, Integer sequenceNumber ) {
-        FinanceProcurementHelper.checkCompleteRequisition( requisitionHeaderService.findRequisitionHeaderByRequestCode( requestCode ) )
-        def requisitionAccounting = requisitionAccountingService.findByRequestCodeItemAndSeq( requestCode, item, sequenceNumber )
-        requisitionAccountingService.delete( [domainModel: requisitionAccounting] )
+    def deletePurchaseRequisitionAccountingInformation(requestCode, Integer item, Integer sequenceNumber) {
+        FinanceProcurementHelper.checkCompleteRequisition(requisitionHeaderService.findRequisitionHeaderByRequestCode(requestCode))
+        def requisitionAccounting = requisitionAccountingService.findByRequestCodeItemAndSeq(requestCode, item, sequenceNumber)
+        requisitionAccountingService.delete([domainModel: requisitionAccounting])
     }
 
     /**
@@ -82,31 +82,31 @@ class RequisitionAccountingCompositeService {
      *
      * @param map the requisition accounting map
      */
-    def updateRequisitionAccounting( accountingDomainModel ) {
+    def updateRequisitionAccounting(accountingDomainModel) {
         // Null or empty check for item number and sequence number.
         FinanceProcurementHelper.checkCompleteRequisition(
-                requisitionHeaderService.findRequisitionHeaderByRequestCode( accountingDomainModel.requisitionAccounting.requestCode ) )
+                requisitionHeaderService.findRequisitionHeaderByRequestCode(accountingDomainModel.requisitionAccounting.requestCode))
 
         if (accountingDomainModel.requisitionAccounting.item == null || accountingDomainModel.requisitionAccounting.sequenceNumber == null) {
             LoggerUtility.error LOGGER, 'Item and Sequence number are required to update the Requisition Accounting information.'
-            throw new ApplicationException( RequisitionAccountingCompositeService,
-                                            new BusinessLogicValidationException(
-                                                    FinanceProcurementConstants.ERROR_MESSAGE_ITEM_SEQUENCE_REQUIRED, [] ) )
+            throw new ApplicationException(RequisitionAccountingCompositeService,
+                    new BusinessLogicValidationException(
+                            FinanceProcurementConstants.ERROR_MESSAGE_ITEM_SEQUENCE_REQUIRED, []))
         }
         Integer item = 0
         if (!accountingDomainModel.requisitionAccounting.item instanceof Integer) {
-            item = Integer.parseInt( accountingDomainModel.requisitionAccounting.item )
+            item = Integer.parseInt(accountingDomainModel.requisitionAccounting.item)
         } else {
             item = accountingDomainModel.requisitionAccounting.item
         }
         Integer sequenceNumber = 0
         if (!accountingDomainModel.requisitionAccounting.sequenceNumber instanceof Integer) {
-            sequenceNumber = Integer.parseInt( accountingDomainModel.requisitionAccounting.sequenceNumber )
+            sequenceNumber = Integer.parseInt(accountingDomainModel.requisitionAccounting.sequenceNumber)
         } else {
             sequenceNumber = accountingDomainModel.requisitionAccounting.sequenceNumber
         }
 
-        def existingAccountingInfo = requisitionAccountingService.findByRequestCodeItemAndSeq( accountingDomainModel.requisitionAccounting.requestCode, item, sequenceNumber )
+        def existingAccountingInfo = requisitionAccountingService.findByRequestCodeItemAndSeq(accountingDomainModel.requisitionAccounting.requestCode, item, sequenceNumber)
         RequisitionAccounting requisitionAccountingRequest = accountingDomainModel.requisitionAccounting
         requisitionAccountingRequest.id = existingAccountingInfo.id
         requisitionAccountingRequest.version = existingAccountingInfo.version
@@ -117,14 +117,14 @@ class RequisitionAccountingCompositeService {
             requisitionAccountingRequest.item = existingAccountingInfo.item
             requisitionAccountingRequest.sequenceNumber = existingAccountingInfo.sequenceNumber
             requisitionAccountingRequest.userId = user.oracleUserName
-            def requisitionAccounting = requisitionAccountingService.update( [domainModel: requisitionAccountingRequest] )
+            def requisitionAccounting = requisitionAccountingService.update([domainModel: requisitionAccountingRequest])
             LoggerUtility.debug LOGGER, "Requisition Accounting information updated " + requisitionAccounting
             return requisitionAccounting
         } else {
             LoggerUtility.error LOGGER, 'User' + user + ' is not valid'
-            throw new ApplicationException( RequisitionAccountingCompositeService,
-                                            new BusinessLogicValidationException(
-                                                    FinanceProcurementConstants.ERROR_MESSAGE_USER_NOT_VALID, [] ) )
+            throw new ApplicationException(RequisitionAccountingCompositeService,
+                    new BusinessLogicValidationException(
+                            FinanceProcurementConstants.ERROR_MESSAGE_USER_NOT_VALID, []))
         }
     }
 
@@ -135,21 +135,74 @@ class RequisitionAccountingCompositeService {
      * @param sequenceNumber Sequence number.
      * @return RequisitionAccounting information.
      */
-    def findByRequestCodeItemAndSeq( requisitionCode, Integer item, Integer sequenceNumber ) {
-        LoggerUtility.debug( LOGGER, String.format( 'Input parameter for findByRequestCodeItemAndSeq :%1s , %2d ,%3d', requisitionCode, item, sequenceNumber ) )
+    def findByRequestCodeItemAndSeq(requisitionCode, Integer item, Integer sequenceNumber) {
+        LoggerUtility.debug(LOGGER, String.format('Input parameter for findByRequestCodeItemAndSeq :%1s , %2d ,%3d', requisitionCode, item, sequenceNumber))
         def dummyPaginationParam = [max: 1, offset: 0]
-        def requisitionAccounting = requisitionAccountingService.findByRequestCodeItemAndSeq( requisitionCode, item, sequenceNumber )
+        def requisitionAccounting = requisitionAccountingService.findByRequestCodeItemAndSeq(requisitionCode, item, sequenceNumber)
+        def financeAccountIndex = financeAccountIndexService.listByIndexCodeOrTitleAndEffectiveDate([coaCode: requisitionAccounting.chartOfAccount, indexCodeTitle: requisitionAccounting.accountIndex], dummyPaginationParam)?.get(0)
+        def financeFund = financeFundCompositeService.findFundListByEffectiveDateAndFundCode([effectiveDate: null, codeTitle: requisitionAccounting.fund, coaCode: requisitionAccounting.chartOfAccount], dummyPaginationParam)?.get(0)
+        def financeOrganization = financeOrganizationCompositeService.findOrganizationListByEffectiveDateAndSearchParam([searchParam: requisitionAccounting.organization, coaCode: requisitionAccounting.chartOfAccount], dummyPaginationParam)?.get(0)
         [accounting: requisitionAccounting, cifoapalp: [
-                chartOfAccount: [code: requisitionAccounting.chartOfAccount, title: requisitionAccounting.chartOfAccount ? chartOfAccountsService.getChartOfAccountByCode( requisitionAccounting.chartOfAccount )?.title : null],
-                index         : [code: requisitionAccounting.accountIndex, title: requisitionAccounting.accountIndex ? accountIndexService.getListByIndexTitleAndEffectiveDate( [coaCode: requisitionAccounting.chartOfAccount, indexCodeTitle: requisitionAccounting.accountIndex], dummyPaginationParam )?.get( 0 )?.title : null],
-                fund          : [code: requisitionAccounting.fund, title: requisitionAccounting.fund ? financeFundService.findFundByEffectiveDateAndFundCode( null, requisitionAccounting.fund, requisitionAccounting.chartOfAccount, dummyPaginationParam )?.get( 0 )?.fundTitle : null],
-                organization  : [code: requisitionAccounting.organization, title: requisitionAccounting.organization ? financeOrganizationCompositeService.
-                        findOrganizationListByEffectiveDateAndSearchParam( [searchParam: requisitionAccounting.organization, coaCode: requisitionAccounting.chartOfAccount], dummyPaginationParam )?.get( 0 )?.orgnTitle : null],
+                chartOfAccount: [code : requisitionAccounting.chartOfAccount,
+                                 title: requisitionAccounting.chartOfAccount ? chartOfAccountsService.getChartOfAccountByCode(requisitionAccounting.chartOfAccount)?.title : null
+                ],
+                index         : [
+                        code                       : requisitionAccounting.accountIndex,
+                        title                      : requisitionAccounting.accountIndex ? financeAccountIndex?.title : null,
+                        accountIndexCode           : financeAccountIndex?.accountIndexCode,
+                        defaultFundOverride        : financeAccountIndex?.defaultFundOverride,
+                        defaultOrganizationOverride: financeAccountIndex?.defaultOrganizationOverride,
+                        defaultAccountOverride     : financeAccountIndex?.defaultAccountOverride,
+                        defaultProgramOverride     : financeAccountIndex?.defaultProgramOverride,
+                        defaultActivityOverride    : financeAccountIndex?.defaultActivityOverride,
+                        defaultLocationOverride    : financeAccountIndex?.defaultLocationOverride,
+                        defaultFundCode            : financeAccountIndex?.defaultFundCode,
+                        defaultFundTitle           : financeAccountIndex?.defaultFundTitle,
+                        defaultOrganizationCode    : financeAccountIndex?.defaultOrganizationCode,
+                        defaultOrganizationTitle   : financeAccountIndex?.defaultOrganizationTitle,
+                        defaultAccountCode         : financeAccountIndex?.defaultAccountCode,
+                        defaultAccountTitle        : financeAccountIndex?.defaultAccountTitle,
+                        defaultProgramCode         : financeAccountIndex?.defaultProgramCode,
+                        defaultProgramTitle        : financeAccountIndex?.defaultProgramTitle,
+                        defaultActivityCode        : financeAccountIndex?.defaultActivityCode,
+                        defaultActivityTitle       : financeAccountIndex?.defaultActivityTitle,
+                        defaultLocationCode        : financeAccountIndex?.defaultLocationCode,
+                        defaultLocationTitle       : financeAccountIndex?.defaultLocationTitle
+                ],
+                fund          : [
+                        code                            : requisitionAccounting.fund,
+                        title                           : requisitionAccounting.fund ? financeFund?.fundTitle : null,
+                        fundCode                        : financeFund?.fundCode,
+                        fundTitle                       : financeFund?.fundTitle,
+                        defaultOrgnCode                 : financeFund?.defaultOrgnCode,
+                        defaultOrgnTitle                : financeFund?.defaultOrgnTitle,
+                        defaultProgCode                 : financeFund?.defaultProgCode,
+                        defaultProgTitle                : financeFund?.defaultProgTitle,
+                        defaultActiveCode               : financeFund?.defaultActiveCode,
+                        defaultActiveTitle              : financeFund?.defaultActiveTitle,
+                        defaultLocationCode             : financeFund?.defaultLocationCode,
+                        defaultLocationTitle            : financeFund?.defaultLocationTitle,
+                        fundTypeDefaultOverrideIndicator: financeFund?.fundTypeDefaultOverrideIndicator
+                ],
+                organization  : [
+                        code                : requisitionAccounting.organization,
+                        title               : requisitionAccounting.organization ? financeOrganization?.orgnTitle : null,
+                        orgnCode            : financeOrganization?.orgnCode,
+                        orgnTitle           : financeOrganization?.orgnTitle,
+                        defaultFundCode     : financeOrganization?.defaultFundCode,
+                        defaultFundTitle    : financeOrganization?.defaultFundTitle,
+                        defaultProgramCode  : financeOrganization?.defaultProgramCode,
+                        defaultProgramTitle : financeOrganization?.defaultProgramTitle,
+                        defaultActivityCode : financeOrganization?.defaultActivityCode,
+                        defaultActivityTitle: financeOrganization?.defaultActivityTitle,
+                        defaultLocationCode : financeOrganization?.defaultLocationCode,
+                        defaultLocationTitle: financeOrganization?.defaultLocationTitle
+                ],
                 account       : [code: requisitionAccounting.account, title: requisitionAccounting.account ? financeAccountCompositeService.getListByAccountOrChartOfAccAndEffectiveDate(
-                        [searchParam: requisitionAccounting.account, coaCode: requisitionAccounting.chartOfAccount], dummyPaginationParam )?.get( 0 )?.title : null],
-                program       : [code: requisitionAccounting.program, title: requisitionAccounting.program ? programService.findByCoaProgramAndEffectiveDate( [coa: requisitionAccounting.chartOfAccount, programCodeDesc: requisitionAccounting.program], dummyPaginationParam )?.get( 0 )?.title : null],
-                activity      : [code: requisitionAccounting.activity, title: requisitionAccounting.activity ? activityService.getListByActivityCodeTitleAndEffectiveDate( [activityCodeTitle: requisitionAccounting.activity, coaCode: requisitionAccounting.chartOfAccount], null, dummyPaginationParam )?.get( 0 )?.title : null],
-                location      : [code: requisitionAccounting.location, title: requisitionAccounting.location ? locationService.getLocationByCodeTitleAndEffectiveDate( [codeTitle: requisitionAccounting.location, coaCode: requisitionAccounting.chartOfAccount], null, dummyPaginationParam )?.get( 0 )?.title : null],
-                project       : [code: requisitionAccounting.project, title: requisitionAccounting.project ? financeProjectCompositeService.getListByProjectAndEffectiveDate( [projectCodeDesc: requisitionAccounting.project, coaCode: requisitionAccounting.chartOfAccount], dummyPaginationParam )?.get( 0 )?.longDescription : null]]]
+                        [searchParam: requisitionAccounting.account, coaCode: requisitionAccounting.chartOfAccount], dummyPaginationParam)?.get(0)?.title : null],
+                program       : [code: requisitionAccounting.program, title: requisitionAccounting.program ? programService.findByCoaProgramAndEffectiveDate([coa: requisitionAccounting.chartOfAccount, programCodeDesc: requisitionAccounting.program], dummyPaginationParam)?.get(0)?.title : null],
+                activity      : [code: requisitionAccounting.activity, title: requisitionAccounting.activity ? activityService.getListByActivityCodeTitleAndEffectiveDate([activityCodeTitle: requisitionAccounting.activity, coaCode: requisitionAccounting.chartOfAccount], null, dummyPaginationParam)?.get(0)?.title : null],
+                location      : [code: requisitionAccounting.location, title: requisitionAccounting.location ? locationService.getLocationByCodeTitleAndEffectiveDate([codeTitle: requisitionAccounting.location, coaCode: requisitionAccounting.chartOfAccount], null, dummyPaginationParam)?.get(0)?.title : null],
+                project       : [code: requisitionAccounting.project, title: requisitionAccounting.project ? financeProjectCompositeService.getListByProjectAndEffectiveDate([projectCodeDesc: requisitionAccounting.project, coaCode: requisitionAccounting.chartOfAccount], dummyPaginationParam)?.get(0)?.longDescription : null]]]
     }
 }
