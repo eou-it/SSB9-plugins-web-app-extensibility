6c918cf09118cf2681647819fac3e4552c474e8e
Arun Upadhya <Arun.Upadhya@ellucian.com>
2015-06-26 19:49:43 +0530

    Cleaning the data for infomation panel data

--- grails-app/services/net/hedtech/banner/finance/requisition/system/FinanceInformationPanelCompositeService.groovy
+++ grails-app/services/net/hedtech/banner/finance/requisition/system/FinanceInformationPanelCompositeService.groovy
@@ -4,7 +4,6 @@
 package net.hedtech.banner.finance.requisition.system
 
 import net.hedtech.banner.finance.requisition.common.FinanceProcurementConstants
-import net.hedtech.banner.finance.system.FinancePendingApproverList
 
 /**
  * Composite service for Information panel.
@@ -22,30 +21,38 @@ class FinanceInformationPanelCompositeService {
      * @return information panel data.
      */
     def getInformationPanelData(status, requestCode) {
-        def informationPanelData = []
-        if (status == FinanceProcurementConstants.REQUISITION_INFO_STATUS_DISAPPROVED) {
-            def comments = financeGeneralTicklerService.findByReferenceNumber(requestCode).collect {
-                [comment: it.comment]
-            }
-            informationPanelData = (comments ? comments : [])
-        } else if (status == FinanceProcurementConstants.REQUISITION_INFO_STATUS_PENDING) {
-            def list = financePendingApproverListService.findByDocumentCode(requestCode).collect {
-                [approverName: it.id.approverName,
-                 description : it.description,
-                 queueId     : it.id.queueId]
-            }
-            informationPanelData = (list ? list : [])
-        } else if (status == FinanceProcurementConstants.REQUISITION_INFO_STATUS_ASSIGNED_TO_BUYER) {
-            def list = financeBuyerVerificationService.findByDocumentCode(requestCode).collect {
-                [buyerCode: it.buyerCode,
-                 buyerName: it.buyerName]
-            }
-            informationPanelData = (list ? list : [])
-        } else if (status == FinanceProcurementConstants.REQUISITION_INFO_STATUS_CONVERTED_TO_PO) {
-            def list = financeRequestPOVerificationService.findByRequestCode(requestCode).collect {
-                [pohdCode: it.pohdCode]
-            }
-            informationPanelData = (list ? list : [])
+        def informationPanelData
+        switch (status) {
+            case FinanceProcurementConstants.REQUISITION_INFO_STATUS_DISAPPROVED:
+                def comments = financeGeneralTicklerService.findByReferenceNumber(requestCode).collect {
+                    [comment: it.comment]
+                }
+                informationPanelData = (comments ? comments : [])
+                break
+            case FinanceProcurementConstants.REQUISITION_INFO_STATUS_PENDING:
+                def list = financePendingApproverListService.findByDocumentCode(requestCode).collect {
+                    [approverName: it.id.approverName,
+                     description : it.description,
+                     queueId     : it.id.queueId]
+                }
+                informationPanelData = (list ? list : [])
+                break
+            case FinanceProcurementConstants.REQUISITION_INFO_STATUS_ASSIGNED_TO_BUYER:
+                def list = financeBuyerVerificationService.findByDocumentCode(requestCode).collect {
+                    [buyerCode: it.buyerCode,
+                     buyerName: it.buyerName]
+                }
+                informationPanelData = (list ? list : [])
+                break
+            case FinanceProcurementConstants.REQUISITION_INFO_STATUS_CONVERTED_TO_PO:
+                def list = financeRequestPOVerificationService.findByRequestCode(requestCode).collect {
+                    [pohdCode: it.pohdCode]
+                }
+                informationPanelData = (list ? list : [])
+                break
+            default:
+                informationPanelData = []
+
         }
         return informationPanelData
     }
