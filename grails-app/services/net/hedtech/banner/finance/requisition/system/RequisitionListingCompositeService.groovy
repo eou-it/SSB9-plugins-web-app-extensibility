321b84ccc93815c2b5dfe1ff7d60967182bdda63
stiwari <shiv.tiwari@ellucian.com>
2015-06-30 15:22:16 +0530

    Organized injected variables

--- grails-app/services/net/hedtech/banner/finance/requisition/system/RequisitionListingCompositeService.groovy
+++ grails-app/services/net/hedtech/banner/finance/requisition/system/RequisitionListingCompositeService.groovy
@@ -18,38 +18,34 @@ import org.springframework.transaction.annotation.Transactional
  * Class for Purchase Requisition Listing Composite
  */
 class RequisitionListingCompositeService {
-    private static final Logger LOGGER = Logger.getLogger(this.class)
+    private static final Logger LOGGER = Logger.getLogger( this.class )
     boolean transactional = true
 
     def springSecurityService
     def requisitionInformationService
     def institutionalDescriptionService
     def currencyFormatService
-    def financeGeneralTicklerService
-    def financePendingApproverListService
-    def financeBuyerVerificationService
-    def financeRequestPOVerificationService
 
     /**
      * Returns list of Requisitions in defined data structure
      * @param buckets
      * @param pagingParams
      */
-    def listRequisitionsByBucket(buckets, pagingParams) {
+    def listRequisitionsByBucket( buckets, pagingParams ) {
         def user = springSecurityService.getAuthentication()?.user
         if (!user.oracleUserName) {
-            LoggerUtility.error(LOGGER, 'User' + user + ' is not valid')
-            throw new ApplicationException(RequisitionListingCompositeService, new BusinessLogicValidationException(
-                    FinanceProcurementConstants.ERROR_MESSAGE_USER_NOT_VALID, []))
+            LoggerUtility.error( LOGGER, 'User' + user + ' is not valid' )
+            throw new ApplicationException( RequisitionListingCompositeService, new BusinessLogicValidationException(
+                    FinanceProcurementConstants.ERROR_MESSAGE_USER_NOT_VALID, [] ) )
         }
         if (buckets?.isEmpty()) {
             buckets = [FinanceProcurementConstants.REQUISITION_LIST_BUCKET_ALL]
         } else {
-            buckets = new ArrayList(buckets).unique()
+            buckets = new ArrayList( buckets ).unique()
         }
         def wrapperList = [];
-        buckets.each() { bucket ->
-            processBucket(wrapperList, bucket, pagingParams, user.oracleUserName)
+        buckets.each() {bucket ->
+            processBucket( wrapperList, bucket, pagingParams, user.oracleUserName )
         }
         return wrapperList
     }
@@ -63,13 +59,13 @@ class RequisitionListingCompositeService {
      * @return
      */
     @Transactional(propagation = Propagation.REQUIRED)
-    public def deriveFormattedAmount(amount, currency, baseCcy) {
+    public def deriveFormattedAmount( amount, currency, baseCcy ) {
         try {
             return amount == null ? amount :
-                    currencyFormatService.format(currency != null ? currency : baseCcy, (amount).toBigDecimal())
+                    currencyFormatService.format( currency != null ? currency : baseCcy, (amount).toBigDecimal() )
         }
         catch (CurrencyNotFoundException cnf) {
-            return FinanceCommonUtility.formatAmountForLocale((amount).toBigDecimal())
+            return FinanceCommonUtility.formatAmountForLocale( (amount).toBigDecimal() )
         }
     }
 
@@ -81,7 +77,7 @@ class RequisitionListingCompositeService {
      * @param pagingParams
      * @return
      */
-    private def processBucket(wrapperList, bucket, pagingParams, user) {
+    private def processBucket( wrapperList, bucket, pagingParams, user ) {
         def draftStatus = [FinanceProcurementConstants.REQUISITION_INFO_STATUS_DRAFT,
                            FinanceProcurementConstants.REQUISITION_INFO_STATUS_DISAPPROVED]
 
@@ -92,26 +88,26 @@ class RequisitionListingCompositeService {
         def pendingStatus = [FinanceProcurementConstants.REQUISITION_INFO_STATUS_PENDING]
         switch (bucket) {
             case FinanceProcurementConstants.REQUISITION_LIST_BUCKET_ALL:
-                wrapperList.add(groupResult(FinanceProcurementConstants.REQUISITION_LIST_BUCKET_DRAFT,
-                        listRequisitions(user, pagingParams, draftStatus)))
-                wrapperList.add(groupResult(FinanceProcurementConstants.REQUISITION_LIST_BUCKET_PENDING,
-                        listRequisitions(user, pagingParams, pendingStatus)))
-                wrapperList.add(groupResult(FinanceProcurementConstants.REQUISITION_LIST_BUCKET_COMPLETE,
-                        listRequisitions(user, pagingParams, completedStatus)))
+                wrapperList.add( groupResult( FinanceProcurementConstants.REQUISITION_LIST_BUCKET_DRAFT,
+                                              listRequisitions( user, pagingParams, draftStatus ) ) )
+                wrapperList.add( groupResult( FinanceProcurementConstants.REQUISITION_LIST_BUCKET_PENDING,
+                                              listRequisitions( user, pagingParams, pendingStatus ) ) )
+                wrapperList.add( groupResult( FinanceProcurementConstants.REQUISITION_LIST_BUCKET_COMPLETE,
+                                              listRequisitions( user, pagingParams, completedStatus ) ) )
                 break
             case FinanceProcurementConstants.REQUISITION_LIST_BUCKET_DRAFT:
-                wrapperList.add(groupResult(bucket, listRequisitions(user, pagingParams, draftStatus)))
+                wrapperList.add( groupResult( bucket, listRequisitions( user, pagingParams, draftStatus ) ) )
                 break
             case FinanceProcurementConstants.REQUISITION_LIST_BUCKET_PENDING:
-                wrapperList.add(groupResult(bucket, listRequisitions(user, pagingParams, pendingStatus)))
+                wrapperList.add( groupResult( bucket, listRequisitions( user, pagingParams, pendingStatus ) ) )
                 break
             case FinanceProcurementConstants.REQUISITION_LIST_BUCKET_COMPLETE:
-                wrapperList.add(groupResult(bucket, listRequisitions(user, pagingParams, completedStatus)))
+                wrapperList.add( groupResult( bucket, listRequisitions( user, pagingParams, completedStatus ) ) )
                 break
             default:
-                LoggerUtility.error(LOGGER, 'Group Type not valid')
-                throw new ApplicationException(RequisitionListingCompositeService, new BusinessLogicValidationException(
-                        FinanceProcurementConstants.ERROR_MESSAGE_INVALID_BUCKET_TYPE, [bucket]))
+                LoggerUtility.error( LOGGER, 'Group Type not valid' )
+                throw new ApplicationException( RequisitionListingCompositeService, new BusinessLogicValidationException(
+                        FinanceProcurementConstants.ERROR_MESSAGE_INVALID_BUCKET_TYPE, [bucket] ) )
         }
     }
 
@@ -121,19 +117,19 @@ class RequisitionListingCompositeService {
      * @param status
      * @return
      */
-    private listRequisitions(oracleUserName, pagingParams, status) {
-        def ret = requisitionInformationService.listRequisitionsByStatus(status, pagingParams, oracleUserName)
-        ret.list = ret.list.collect() { RequisitionInformation it ->
+    private listRequisitions( oracleUserName, pagingParams, status ) {
+        def ret = requisitionInformationService.listRequisitionsByStatus( status, pagingParams, oracleUserName )
+        ret.list = ret.list.collect() {RequisitionInformation it ->
             [id             : it.id,
              version        : it.version,
-             amount         : deriveFormattedAmount(it.amount, it.currency, institutionalDescriptionService.findByKey().baseCurrCode),
+             amount         : deriveFormattedAmount( it.amount, it.currency, institutionalDescriptionService.findByKey().baseCurrCode ),
              coasCode       : it.coasCode,
              requestDate    : it.requestDate,
              requisitionCode: it.requisitionCode,
              transactionDate: it.transactionDate,
              vendorName     : it.vendorName,
              organization   : it.organizationTitle,
-             status         : MessageHelper.message('purchaseRequisition.status.' + it.status),
+             status         : MessageHelper.message( 'purchaseRequisition.status.' + it.status ),
              infoStatus     : it.status]
         }
         return ret
@@ -145,7 +141,7 @@ class RequisitionListingCompositeService {
      * @param records
      * @return
      */
-    private def groupResult(groupType, records) {
+    private def groupResult( groupType, records ) {
         [category: groupType, count: records.count, list: records.list]
     }
 }
