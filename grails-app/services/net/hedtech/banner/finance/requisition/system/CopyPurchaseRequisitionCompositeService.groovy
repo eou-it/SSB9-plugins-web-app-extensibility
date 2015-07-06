90a74099ed8b6bd3a4bd752add42aca3f38a06bd
stiwari <shiv.tiwari@ellucian.com>
2015-07-06 14:01:19 +0530

    Improvement

--- grails-app/services/net/hedtech/banner/finance/requisition/system/CopyPurchaseRequisitionCompositeService.groovy
+++ grails-app/services/net/hedtech/banner/finance/requisition/system/CopyPurchaseRequisitionCompositeService.groovy
@@ -9,12 +9,16 @@ import net.hedtech.banner.finance.requisition.common.FinanceProcurementConstants
 import net.hedtech.banner.finance.requisition.util.FinanceProcurementSQLUtils
 import net.hedtech.banner.finance.util.LoggerUtility
 import org.apache.log4j.Logger
+import org.hibernate.HibernateException
+import org.hibernate.Session
+
+import java.sql.SQLException
 
 /**
  * The service class which is used to have methods for copy purchase requisition.
  */
 class CopyPurchaseRequisitionCompositeService {
-    private static final def LOGGER = Logger.getLogger(this.getClass())
+    private static final def LOGGER = Logger.getLogger( this.getClass() )
     boolean transactional = true
 
     def sessionFactory
@@ -23,28 +27,38 @@ class CopyPurchaseRequisitionCompositeService {
     /**
      * This method is used to copy the requisition.
      */
-    def copyRequisition(requestCode) {
-        def header = requisitionHeaderService.findRequisitionHeaderByRequestCode(requestCode)
+    def copyRequisition( requestCode ) {
+        def header = requisitionHeaderService.findRequisitionHeaderByRequestCode( requestCode )
         if (header && header.completeIndicator) {
+            Session session
             try {
-                def session = sessionFactory.getCurrentSession()
-                session.createSQLQuery(FinanceProcurementSQLUtils.getUpdateReqNextQuery()).executeUpdate()
-                def nextDocCode = session.createSQLQuery(FinanceProcurementSQLUtils.getSelectGeneratedReqCodeQuery()).list()[0]
-                session.createSQLQuery(FinanceProcurementSQLUtils.getCopyRequisitionQuery())
-                        .setParameter('nextDocCode', nextDocCode)
-                        .setParameter('oldDocCode', requestCode)
+                session = sessionFactory.getCurrentSession()
+                session.createSQLQuery( FinanceProcurementSQLUtils.getUpdateReqNextQuery() ).executeUpdate()
+                def nextDocCode = session.createSQLQuery( FinanceProcurementSQLUtils.getSelectGeneratedReqCodeQuery() ).list()[0]
+                session.createSQLQuery( FinanceProcurementSQLUtils.getCopyRequisitionQuery() )
+                        .setParameter( 'nextDocCode', nextDocCode )
+                        .setParameter( 'oldDocCode', requestCode )
                         .executeUpdate()
                 return nextDocCode
-            } catch (Exception e) {
-                LoggerUtility.error(LOGGER, "Error While Copy Requisition $header.requestCode")
-                throw new ApplicationException(CopyPurchaseRequisitionCompositeService, e)
+            } catch (HibernateException | SQLException e) {
+                LoggerUtility.error( LOGGER, "Error While Copy Requisition $header.requestCode" )
+                throw new ApplicationException( CopyPurchaseRequisitionCompositeService, e )
+            }
+            finally {
+                try {
+                    session?.close()
+                }
+                catch (HibernateException e) {
+                    LoggerUtility.error( LOGGER, "Error While Copy Requisition $header.requestCode" )
+                    throw new ApplicationException( CopyPurchaseRequisitionCompositeService, e )
+                }
             }
         } else {
-            LoggerUtility.error(LOGGER, "Only completed requisition can be copied = $header.requestCode")
+            LoggerUtility.error( LOGGER, "Only completed requisition can be copied = $header.requestCode" )
             throw new ApplicationException(
                     CopyPurchaseRequisitionCompositeService,
                     new BusinessLogicValidationException(
-                            FinanceProcurementConstants.ERROR_MESSAGE_COMPLETED_REQUISITION_IS_REQUIRED, []))
+                            FinanceProcurementConstants.ERROR_MESSAGE_COMPLETED_REQUISITION_IS_REQUIRED, [] ) )
         }
     }
 }
