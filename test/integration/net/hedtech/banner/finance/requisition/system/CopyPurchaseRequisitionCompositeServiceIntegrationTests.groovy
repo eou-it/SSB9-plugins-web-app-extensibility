71913fbbe355dff64ea8b5e5b725c32c34285121
stiwari <shiv.tiwari@ellucian.com>
2015-07-14 13:50:36 +0530

    Test Fix

--- test/integration/net/hedtech/banner/finance/requisition/system/CopyPurchaseRequisitionCompositeServiceIntegrationTests.groovy
+++ test/integration/net/hedtech/banner/finance/requisition/system/CopyPurchaseRequisitionCompositeServiceIntegrationTests.groovy
@@ -38,7 +38,7 @@ class CopyPurchaseRequisitionCompositeServiceIntegrationTests extends BaseIntegr
      */
     @Test
     public void testCopyRequisition() {
-        assertNotNull(copyPurchaseRequisitionCompositeService.copyRequisition('R0001195'))
+        assertNotNull(copyPurchaseRequisitionCompositeService.copyRequisition('R0002230'))
     }
 
     /**
@@ -47,7 +47,7 @@ class CopyPurchaseRequisitionCompositeServiceIntegrationTests extends BaseIntegr
     @Test
     public void testCopyRequisitionFailCase() {
         try {
-            copyPurchaseRequisitionCompositeService.copyRequisition('R0001195')
+            copyPurchaseRequisitionCompositeService.copyRequisition('R0002210')
         } catch (ApplicationException e) {
             assertApplicationException e, (FinanceProcurementConstants.ERROR_MESSAGE_COMPLETED_REQUISITION_IS_REQUIRED)
         }
