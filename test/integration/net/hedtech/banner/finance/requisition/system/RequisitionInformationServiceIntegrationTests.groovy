5faa8b1b6ffceda0b5f06d229c21a06f9c65a05a
stiwari <shiv.tiwari@ellucian.com>
2015-07-27 21:04:03 +0530

    Test Fix

--- test/integration/net/hedtech/banner/finance/requisition/system/RequisitionInformationServiceIntegrationTests.groovy
+++ test/integration/net/hedtech/banner/finance/requisition/system/RequisitionInformationServiceIntegrationTests.groovy
@@ -44,7 +44,7 @@ class RequisitionInformationServiceIntegrationTests extends BaseIntegrationTestC
     @Test
     void fetchRequisitionsCountByStatus() {
         def count = requisitionInformationService.fetchRequisitionsCountByStatus( [FinanceProcurementConstants.REQUISITION_INFO_STATUS_DRAFT], null )
-        assertTrue count > 0
+        assertTrue count.get(FinanceProcurementConstants.REQUISITION_INFO_STATUS_DRAFT) > 0
     }
 
     /**
@@ -53,7 +53,7 @@ class RequisitionInformationServiceIntegrationTests extends BaseIntegrationTestC
     @Test
     void fetchRequisitionsCountByStatusWithInvalidUser() {
         def count = requisitionInformationService.fetchRequisitionsCountByStatus( [FinanceProcurementConstants.REQUISITION_INFO_STATUS_DRAFT], 'Invalid' )
-        assertTrue count == 0
+        assertTrue count.get(FinanceProcurementConstants.REQUISITION_INFO_STATUS_DRAFT) == 0
     }
 
     /**
