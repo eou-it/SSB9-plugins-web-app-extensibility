278b18f1bd4d66c545e71d6e3f8deef67c1f87f8
stiwari <shiv.tiwari@ellucian.com>
2015-07-30 14:19:05 +0530

    PDF generation test fix

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,6 +1,6 @@
 /*******************************************************************************
-  Copyright 2015 Ellucian Company L.P. and its affiliates.
-*******************************************************************************/
+ Copyright 2015 Ellucian Company L.P. and its affiliates.
+ *******************************************************************************/
 
 tomcat.deploy.username = "manager"
 tomcat.deploy.password = "manager!"
@@ -16,13 +16,13 @@ grails.project.test.reports.dir = "target/test-reports"
 grails.plugin.location.'banner-core' = "../banner_core.git"
 grails.plugin.location.'banner-codenarc' = "../banner_codenarc.git"
 grails.plugin.location.'i18n-core' = "../i18n_core.git"
-grails.plugin.location.'banner-seeddata-catalog'="../banner_seeddata_catalog.git"
-grails.plugin.location.'banner-finance-validation'="../banner_finance_validation.git"
-grails.plugin.location.'banner-finance-validation_common'="../banner_finance_validation_common.git"
-grails.plugin.location.'banner_general_validation_common'="../banner_general_validation_common.git"
+grails.plugin.location.'banner-seeddata-catalog' = "../banner_seeddata_catalog.git"
+grails.plugin.location.'banner-finance-validation' = "../banner_finance_validation.git"
+grails.plugin.location.'banner-finance-validation_common' = "../banner_finance_validation_common.git"
+grails.plugin.location.'banner_general_validation_common' = "../banner_general_validation_common.git"
 
-grails.war.resources = { stagingDir ->
-    delete(file: "${stagingDir}/WEB-INF/lib/ojdbc6.jar")
+grails.war.resources = {stagingDir ->
+    delete( file: "${stagingDir}/WEB-INF/lib/ojdbc6.jar" )
 }
 
 grails.project.dependency.resolver = "ivy"
@@ -49,7 +49,7 @@ grails.project.dependency.resolution = {
     plugins {
         compile ":spring-security-core:1.2.7.3"
         test ':code-coverage:1.2.5'
-        runtime  ":hibernate:3.6.10.10"
+        runtime ":hibernate:3.6.10.10"
         compile ":tomcat:7.0.52.1"
         compile ':resources:1.2.7' // If the functional-test plugin is being used
         compile ":functional-test:2.0.0" // If the functional-test plugin is being used
@@ -58,13 +58,16 @@ grails.project.dependency.resolution = {
     dependencies {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
         // Note: elvyx-1.0.24_beta.jar remains in the lib/ directory of the project as it is not available in a public repo due to licensing issues.
-        test (
+        test(
                 "net.hedtech.banner.pdf:banner_pdf_generator:1.0",
                 "org.apache.avalon.framework:avalon-framework-api:4.3.1",
                 "org.apache.avalon.framework:avalon-framework-impl:4.3.1",
                 "org.json:json:20090211",
                 "org.apache.xmlgraphics:fop:1.1"
-        )
+        ) {
+            excludes 'xml-apis'
+        }
+
     }
 
 }
