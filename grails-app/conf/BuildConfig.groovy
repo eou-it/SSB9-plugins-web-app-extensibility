cb69a6b05285eace47cb5145b5a8fc612337778f
stiwari <shiv.tiwari@ellucian.com>
2016-02-17 16:52:02 +0530

    Pdf change

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -14,14 +14,14 @@ grails.project.test.reports.dir = "target/test-reports"
 // "Cannot cast object 'oracle.jdbc.driver.T4CConnection@6469adc7'... to class 'oracle.jdbc.OracleConnection'
 
 grails.plugin.location.'banner-codenarc' = "../banner_codenarc.git"
-grails.plugin.location.'banner-finance-validation'="../banner_finance_validation.git"
-grails.plugin.location.'banner-finance-validation_common'="../banner_finance_validation_common.git"
-grails.plugin.location.'banner_general_validation_common'="../banner_general_validation_common.git"
-grails.plugin.location.'banner_general_person'="../banner_general_person.git"
-grails.plugin.location.'banner_document_management'="../banner_document_management.git"
+grails.plugin.location.'banner-finance-validation' = "../banner_finance_validation.git"
+grails.plugin.location.'banner-finance-validation_common' = "../banner_finance_validation_common.git"
+grails.plugin.location.'banner_general_validation_common' = "../banner_general_validation_common.git"
+grails.plugin.location.'banner_general_person' = "../banner_general_person.git"
+grails.plugin.location.'banner_document_management' = "../banner_document_management.git"
 
-grails.war.resources = { stagingDir ->
-    delete(file: "${stagingDir}/WEB-INF/lib/ojdbc6.jar")
+grails.war.resources = {stagingDir ->
+    delete( file: "${stagingDir}/WEB-INF/lib/ojdbc6.jar" )
 }
 
 grails.project.dependency.resolver = "maven"
@@ -31,14 +31,14 @@ grails.project.dependency.resolution = {
     inherits "global" // inherit Grails' default dependencies
     log "warn"        // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
 
-	repositories {
-		if ( System.properties[ 'PROXY_SERVER_NAME' ] ) {
-		mavenRepo "${System.properties['PROXY_SERVER_NAME']}"
-		}
-		grailsCentral()
-		mavenCentral()
-		mavenRepo "http://repository.jboss.org/maven2/"
-	}
+    repositories {
+        if (System.properties['PROXY_SERVER_NAME']) {
+            mavenRepo "${System.properties['PROXY_SERVER_NAME']}"
+        }
+        grailsCentral()
+        mavenCentral()
+        mavenRepo "http://repository.jboss.org/maven2/"
+    }
 
     plugins {
     }
@@ -47,7 +47,17 @@ grails.project.dependency.resolution = {
     dependencies {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
         // Note: elvyx-1.0.24_beta.jar remains in the lib/ directory of the project as it is not available in a public repo due to licensing issues.
-        test (  "net.hedtech.banner.pdf:banner_pdf_generator:1.0",
+        test(
+                "net.hedtech.banner.pdf:banner_pdf_generator:1.0",
+                "org.apache.avalon.framework:avalon-framework-api:4.3.1",
+                "org.apache.avalon.framework:avalon-framework-impl:4.3.1",
+                "org.json:json:20090211",
+                "org.apache.xmlgraphics:fop:1.1"
+
+        ) {
+            excludes 'xml-apis'
+        }
+        /*test (  "net.hedtech.banner.pdf:banner_pdf_generator:1.0",
                 "org.apache.xmlgraphics:fop:1.1",
                 "org.apache.xmlgraphics:batik-transcoder:1.8",
                 "org.apache.xmlgraphics:batik-codec:1.8",
@@ -70,7 +80,7 @@ grails.project.dependency.resolution = {
                 "org.apache.avalon.framework:avalon-framework-impl:4.3.1",
                 "xml-apis:xml-apis-ext:1.3.04",
                 "org.json:json:20090211" )
-                {transitive = false}
+                {transitive = false}*/
     }
 
 }
