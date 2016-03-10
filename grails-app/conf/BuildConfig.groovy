6d79c7d96087448f2f6cbc3368bd7af8fef59052
Sandeep Vedavyas <sandeep.vedavyas@ellucian.com>
2016-03-10 15:29:07 +0530

    made copyright year change to 2016

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,5 +1,5 @@
 /*******************************************************************************
- Copyright 2015 Ellucian Company L.P. and its affiliates.
+ Copyright 2016 Ellucian Company L.P. and its affiliates.
  *******************************************************************************/
 
 tomcat.deploy.username = "manager"
