cb6bdcffcc0c32b640f66402ee299caa9b7bb225
VinayHS <VinayHS@Vinay-HS.local>
2017-03-22 14:41:30 +0530

    Update version number to 9.22.1

--- WebAppExtensibilityGrailsPlugin.groovy
+++ WebAppExtensibilityGrailsPlugin.groovy
@@ -4,7 +4,7 @@
 
 class WebAppExtensibilityGrailsPlugin {
     // the plugin version
-    def version = "9.17.1"
+    def version = "9.22.1"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.2 > *"
     // resources that are excluded from plugin packaging
