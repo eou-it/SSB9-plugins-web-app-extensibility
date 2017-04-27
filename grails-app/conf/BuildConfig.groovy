d8d3143bab8fec822dc4ed3a01bf03e6363ea976
jeetp <jeet.parmar@ellucian.com>
2017-04-28 00:31:13 +0530

    fixed performance issues and testcase failing issue

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -14,9 +14,9 @@ grails.project.test.reports.dir = "target/test-reports"
 // "Cannot cast object 'oracle.jdbc.driver.T4CConnection@6469adc7'... to class 'oracle.jdbc.OracleConnection'
 
 grails.plugin.location.'banner-codenarc' = "../banner_codenarc.git"
+
+
 grails.plugin.location.'banner-finance-validation' = "../banner_finance_validation.git"
-grails.plugin.location.'banner-finance-validation_common' = "../banner_finance_validation_common.git"
-grails.plugin.location.'banner_general_validation_common' = "../banner_general_validation_common.git"
 grails.plugin.location.'banner_general_person' = "../banner_general_person.git"
 grails.plugin.location.'banner_document_management' = "../banner_document_management.git"
 
@@ -48,31 +48,43 @@ grails.project.dependency.resolution = {
     dependencies {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
         // Note: elvyx-1.0.24_beta.jar remains in the lib/ directory of the project as it is not available in a public repo due to licensing issues.
-        test("org.apache.xmlgraphics:fop:1.1",
-              "org.apache.xmlgraphics:xmlgraphics-commons:1.5",
-              "org.apache.xmlgraphics:batik-svg-dom:1.7",
-              "org.apache.xmlgraphics:batik-anim:1.7",
-              "org.apache.xmlgraphics:batik-css:1.7",
-              "org.apache.xmlgraphics:batik-dom:1.7",
-              "org.apache.xmlgraphics:batik-parser:1.7",
-              "org.apache.xmlgraphics:batik-util:1.7",
-              "org.apache.xmlgraphics:batik-bridge:1.7",
-              "org.apache.xmlgraphics:batik-script:1.7",
-              "org.apache.xmlgraphics:batik-xml:1.7",
-              "org.apache.xmlgraphics:batik-awt-util:1.7",
-              "org.apache.xmlgraphics:batik-gvt:1.7",
-              "org.apache.xmlgraphics:batik-transcoder:1.7",
-              "org.apache.xmlgraphics:batik-svggen:1.7",
-              "org.apache.xmlgraphics:batik-extension:1.7",
-              "org.apache.xmlgraphics:batik-ext:1.7",
-              "avalon-framework:avalon-framework-api:4.2.0",
-              "avalon-framework:avalon-framework-impl:4.2.0",
-              "xml-apis:xml-apis:1.3.04",
-              "xml-apis:xml-apis-ext:1.3.04",
-              "xalan:xalan:2.6.0",
-              "commons-logging:commons-logging:1.0.4",
-              "commons-io:commons-io:1.3.1",
-              "org.json:json:20090211" )
+        /*test(
+                "net.hedtech.banner.pdf:banner_pdf_generator:1.0",
+                "org.apache.avalon.framework:avalon-framework-api:4.3.1",
+                "org.apache.avalon.framework:avalon-framework-impl:4.3.1",
+                "org.json:json:20090211",
+                "org.apache.xmlgraphics:batik-ext:1.7",
+                "org.apache.xmlgraphics:fop:1.1"
+
+        ) {
+            excludes 'xml-apis'
+        }*/
+        test("net.hedtech.banner.pdf:banner_pdf_generator:1.0",
+                "org.apache.xmlgraphics:fop:1.1",
+                "org.apache.xmlgraphics:xmlgraphics-commons:1.5",
+                "org.apache.xmlgraphics:batik-svg-dom:1.7",
+                "org.apache.xmlgraphics:batik-anim:1.7",
+                "org.apache.xmlgraphics:batik-css:1.7",
+                "org.apache.xmlgraphics:batik-dom:1.7",
+                "org.apache.xmlgraphics:batik-parser:1.7",
+                "org.apache.xmlgraphics:batik-util:1.7",
+                "org.apache.xmlgraphics:batik-bridge:1.7",
+                "org.apache.xmlgraphics:batik-script:1.7",
+                "org.apache.xmlgraphics:batik-xml:1.7",
+                "org.apache.xmlgraphics:batik-awt-util:1.7",
+                "org.apache.xmlgraphics:batik-gvt:1.7",
+                "org.apache.xmlgraphics:batik-transcoder:1.7",
+                "org.apache.xmlgraphics:batik-svggen:1.7",
+                "org.apache.xmlgraphics:batik-extension:1.7",
+                "org.apache.xmlgraphics:batik-ext:1.7",
+                "avalon-framework:avalon-framework-api:4.2.0",
+                "avalon-framework:avalon-framework-impl:4.2.0",
+                "xml-apis:xml-apis:1.3.04",
+                "xml-apis:xml-apis-ext:1.3.04",
+                "xalan:xalan:2.6.0",
+                "commons-logging:commons-logging:1.0.4",
+                "commons-io:commons-io:1.3.1",
+                "org.json:json:20090211" )
                 {transitive = false}
     }
 
