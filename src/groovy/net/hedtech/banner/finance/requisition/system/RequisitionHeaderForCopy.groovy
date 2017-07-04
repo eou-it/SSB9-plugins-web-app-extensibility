b09b1d06328ae3c3b4d461e0529de61775327bba
Tiwari Shiv <shiv.tiwari@ellucian.com>
2017-07-04 20:34:32 +0530

    reverted the change for CacheMode.get

--- src/groovy/net/hedtech/banner/finance/requisition/system/RequisitionHeaderForCopy.groovy
+++ src/groovy/net/hedtech/banner/finance/requisition/system/RequisitionHeaderForCopy.groovy
@@ -7,7 +7,6 @@ package net.hedtech.banner.finance.requisition.system
 import groovy.transform.EqualsAndHashCode
 import groovy.transform.ToString
 import net.hedtech.banner.finance.requisition.common.FinanceProcurementConstants
-import org.hibernate.CacheMode
 import org.hibernate.Session
 import org.hibernate.annotations.Type
 
@@ -281,7 +280,6 @@ class RequisitionHeaderForCopy implements Serializable {
                     .setMaxResults( pagingParams.max )
                     .setFirstResult( pagingParams.offset )
                     .setCacheable( true )
-                    .setCacheMode(CacheMode.GET)
                     .setCacheRegion( 'financeProcurementTablesCacheRegion' )
                     .list()
         }
