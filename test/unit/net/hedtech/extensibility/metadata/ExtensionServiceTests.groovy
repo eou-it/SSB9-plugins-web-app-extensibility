1c4f57bca56de9abbfe03193865eeeae2b818136
gurunath_dk <gurunat.dk@ellucian.com>
2018-06-29 10:06:46 +0530

    EXTZ-2408 Adding Test Cases for webapp Extensibility

--- test/unit/net/hedtech/extensibility/metadata/ExtensionServiceTests.groovy
+++ test/unit/net/hedtech/extensibility/metadata/ExtensionServiceTests.groovy
@@ -1,29 +1,104 @@
 /*******************************************************************************
- Copyright 2015 Ellucian Company L.P. and its affiliates.
+ Copyright 2015-2018 Ellucian Company L.P. and its affiliates.
  ******************************************************************************/
 
 package net.hedtech.extensibility.metadata
 
-import grails.test.mixin.TestFor
+import grails.converters.JSON
+import grails.test.mixin.TestMixin
+import grails.test.mixin.web.ControllerUnitTestMixin
+import org.codehaus.groovy.grails.web.json.JSONObject
+import spock.lang.Specification
 
-/**
- * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
- */
-@TestFor(ExtensionService)
-class ExtensionServiceTests {
+@TestMixin(ControllerUnitTestMixin)
+class ExtensionServiceTests extends Specification{
+
+    def jsonData = '{' +
+            '   "sections":[' +
+            '      {' +
+            '         "name":"extzToolList",' +
+            '         "exclude":false,' +
+            '         "fields":[' +
+            '            {' +
+            '               "name":"about",' +
+            '               "exclude":false' +
+            '            },' +
+            '            {' +
+            '               "name":"uploadProperties",' +
+            '               "exclude":false' +
+            '            }' +
+            '         ]' +
+            '      }' +
+            '   ]' +
+            '}'
 
     def extensionService = new ExtensionService()
-    def params = [unitTest: true, dummy: 1]
+
+    def params = [unitTest: true, dummy: 1, application:"banner_extensibility", page:"Test",
+                  "metadata":jsonData]
+    def static extensionsPath = grails.util.Holders.getConfig().webAppExtensibility.locations.extensions
 
 
-    void testList() {
+    def setup() {
+        def folder = new File( "${extensionsPath}/${params.application}/${params.page}.json" )
+        if( !folder.exists() ) {
+            folder.getParentFile().mkdirs();
+            folder.createNewFile();
+            folder << jsonData.toString()
+        }
+
+    }
+
+    def cleanup(){
+        def file = new File( "${extensionsPath}/${params.application}/${params.page}.json" )
+        if(file.exists()){
+            file.delete()
+        }
+    }
+
+    void "test for list"() {
+        when:
         def result = extensionService.list(params)
-        assertNotNull(result)
+        then:
+        result !=null
     }
 
 
-    void testCount() {
+    void "test for count"() {
+        when:
         def count = extensionService.count(params)
-        assertEquals(count, 1)
+        then:
+        count == 1
     }
+
+    void "test for save"(){
+        given:
+        JSONObject row = new JSONObject()
+        Map data = new HashMap()
+        data.put("name","TestPage")
+        row.put("sections",data)
+        Map content = ["TestData":"Test Data output"]
+        params << ["metadata":JSON.parse("{'sections':[{'name':'extzToolList','exclude':false}]}")]
+        //params << ["metadata":jsonSlurper.parseText(jsonData)]
+        when:
+        def res = extensionService.create(params,content)
+        then:
+        noExceptionThrown()
+        when:
+        params <<["application":null]
+        extensionService.create(params,content)
+        then:
+        thrown Exception
+
+    }
+
+    void "loadExtensionsJSON" (){
+        given:
+        def requestURI = "V${params.application}/${params.page}.json"
+        when:
+        ExtensionService.loadExtensionsJSON(requestURI)
+        then:
+        noExceptionThrown()
+    }
+
 }
