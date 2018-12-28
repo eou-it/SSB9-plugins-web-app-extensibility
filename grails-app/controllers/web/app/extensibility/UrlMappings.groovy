3de79a52cbdde4d3340e2e02b5fe1aa9afa22c43
Ankur Jain <ankur.jain@ellucian.com>
2018-12-28 12:06:07 +0530

    Removed extra UrlMappings.groovy class and moved relevant content to main UrlMappings.groovy class HRU-8280

--- grails-app/controllers/web/app/extensibility/UrlMappings.groovy
+++ grails-app/controllers/web/app/extensibility/UrlMappings.groovy
@@ -9,8 +9,36 @@ class UrlMappings {
             }
         }
 
-        "/"(view:"/index")
-        "500"(view:'/error')
-        "404"(view:'/notFound')
+        //read only restful api
+        "/internal/$pluralizedResourceName/$id"(controller: 'restfulApi') {
+            action = [GET: "show"]
+            parseRequest = false
+            constraints {
+                // to constrain the id to numeric, uncomment the following:
+                // id matches: /\d+/
+            }
+        }
+        "/internal/$pluralizedResourceName"(controller: 'restfulApi') {
+            action = [GET: "list"]
+            parseRequest = false
+        }
+        //secured restful api requiring the webtailor admin role
+        "/webadmin/$pluralizedResourceName/$id"(controller: 'restfulApi') {
+            action = [GET   : "show", PUT: "update",
+                      DELETE: "delete"]
+            parseRequest = false
+            constraints {
+                // to constrain the id to numeric, uncomment the following:
+                // id matches: /\d+/
+            }
+        }
+        "/webadmin/$pluralizedResourceName"(controller: 'restfulApi') {
+            action = [GET: "list", POST: "create"]
+            parseRequest = false
+        }
+
+//        "/"(view:"/index")
+//        "500"(view:'/error')
+//        "404"(view:'/notFound')
     }
 }
